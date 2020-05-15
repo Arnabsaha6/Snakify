b443aa168a67186e287068c425704f9ec0a066b5
Arnab Saha <42444789+Arnabsaha6@users.noreply.github.com>
2020-05-15 22:46:58 +0530

    Create Queenmove.py

---
+++ Queenmove.py
@@ -0,0 +1,15 @@
+Queen move
+Statement
+Chess queen moves horizontally, vertically or diagonally to any number of cells. Given two different cells of the chessboard, determine whether a queen can go from the first cell to the second in one move.
+The program receives the input of four numbers from 1 to 8, each specifying the column and row number, first two - for the first cell, and then the last two - for the second cell. The program should output YES if a queen can go from the first cell to the second in one move, or NO otherwise.
+
+Code:
+col = int(input())
+row = int(input())
+colN = int(input())
+rowN = int(input())
+
+if col == colN or row == rowN or abs(col - colN) == abs(row - rowN):
+  print ("YES")
+else:
+  print ("NO")
