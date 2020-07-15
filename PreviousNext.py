c803e6b34070c0efef8924b88069a55a0a7a27e0
Arnab Saha <42444789+Arnabsaha6@users.noreply.github.com>
2020-07-15 22:42:33 +0530

    Create PreviousNext.py

---
+++ PreviousNext.py
@@ -0,0 +1,6 @@
+Code:
+
+n = int(input())
+
+print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
+print('The previous number for the number ' + str(n) + ' is ' + str(n - 1) + '.')
